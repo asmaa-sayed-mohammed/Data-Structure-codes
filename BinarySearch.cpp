80a2993f168b805dbac95219b46d79bbb362c9d0
Asmaa <saiedasmaa590@gmail.com>
2025-06-09 02:49:31 +0300

    BST and stack operations

---
+++ BinarySearch.cpp
@@ -0,0 +1,251 @@
+//
+// Created by Mohamed.S on 6/7/2025.
+//
+#include <iostream>
+#include <queue>
+using namespace std;
+
+//// playlists to study and understand the tree:
+
+/*
+ * Adel Naseem --> Arabic there is some videos without coding
+ * Hard-code -->Arabic with trace the code
+ * mycodeschool --> English without trace the code
+ */
+
+//// Introduction to Binary tree
+
+/*the basic difference between BT and GT :
+ * BT must have at most 2 children for each node but GT can have any number of children for each node
+ */
+//=============================================================================================================
+
+//// complete binary tree:
+/*
+ * all levels except the last level are completely filled and all nodes are left as possible
+ * -------------------------------------------------------------------------------------------------
+ * if we start leve as 0, 1, 2 , ... :
+ * we can say that max number of nodes in each level is 2*i (i is the number of level)
+ * -------------------------------------------------------------------------------------------------
+ * but if we start number of levels as 1, 2, 3, 4, ...
+ * so we can say that the max number of nodes in level is 2*(i - 1)
+ * --------------------------------------------------------------------------------------------------------
+ * if we have n as a number of node so we can say that the height of tree is:
+ * floor(log n to base 2)
+ */
+//=============================================================================================================
+
+//// Perfect binary tree:
+/*
+ * all levels in the tree are completely full no exception for the last level
+ * ----------------------------------------------------------------------------
+ * max number of nodes in the tree(height of the tree = h):
+ * = 2*0 + 2*1 + 2*2 + 2*3 +....... + 2*h
+ * = (2*(h+1)) - 1
+ * or:
+ * = 2*1 + 2*2 + 2*3 + 2*4 +..............+ 2*h
+ * = (2*h) - 1
+ */
+
+/// Binary Search Tree
+/*
+ * all nodes in left subtree must be less than the root and all elements in right subtree must be greater than the root.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * complexity of search, insertion and deletion:
+ * ---------------------------------------------
+ * in average case (the tree is balanced):
+ * insertion, deletion and search: o(log n to base 2)
+ * --------------------------------
+ * in worst case(each node has only one child it will be like the linked list):
+ * insertion, deletion and search: o(n)
+ */
+//====================================================================================================================
+//// implementation of BST
+//---------------------------
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+};
+
+class BST_Operations {
+public:
+    // create the root to access the tree
+     Node *root = nullptr;
+        queue<Node*> q;
+    void insertion(int data) {
+        // create a new node and let data = data from function and left, right = null
+        Node *newNode = new Node();
+        newNode->data = data;
+        newNode->left = nullptr;
+        newNode->right = nullptr;
+        // create a new pointer to trace on tree
+        Node *current = root;
+        // if tree is empty (root = null) let root = newNode
+        if (root == nullptr) {
+            root = newNode;
+        }else {
+            //loop through the tree until you find a true position
+            while (true) {
+                // if data < current value and there is no left child let the left child = new node
+                if (current -> left == nullptr && data < current -> data) {
+                    current->left = newNode;
+                    break;
+                }
+                // if data > current value and there is no right child let the right child = new node
+                else if (current -> right == nullptr && data > current -> data) {
+                    current->right = newNode;
+                    break;
+                }
+                // if the data < current data and there is a left child update the current pointer to be the left child
+                else if (current -> left != nullptr && data < current -> data) {
+                    current = current -> left;
+                }
+                // if the data > current data and there is a right child update the current pointer to be the right child
+                else if (current -> right != nullptr && data > current -> data) {
+                    current = current -> right;
+                }
+            }
+        }
+    }
+    ///find the maximum node in binary search tree
+    // the maximum node will be in the right as far as possible
+    Node *maxNode(Node *node) {
+        node = root;
+        while (node != nullptr) {
+            node = node -> right;
+        }
+        return node;
+    }
+    ///find the minimum node in binary search tree
+    // the minimum node will be in the left as far as possible
+    void minNode(Node *node) {
+        node = root;
+        while (node != nullptr) {
+            node = node -> left;
+        }
+        cout << node -> data;
+    }
+    /// find the height of tree
+    // the height is the most depth of node we can calculate it as the maximum between the left and right subtree + 1
+    int findHeight(Node *node) {
+        if (node == nullptr) {
+            return -1;
+        }else {
+            int left = findHeight(node->left);
+           int right = findHeight(node->right);
+            return max(left, right) + 1;
+        }
+    }
+    /// traverse the tree
+    // breadth-first (print each level)
+    void breadthFirst(Node *root) {
+        if (root == nullptr) {
+            cout << "Empty Tree";
+        }else {
+            q.push(root);
+            Node *current = root;
+            while (!q.empty()) {
+                current = q.front();
+                cout << current -> data;
+                q.pop();
+                if (current -> left != nullptr) {
+                    q.push(current -> left);
+                }
+                if (current -> right != nullptr) {
+                    q.push(current -> right);
+                }
+            }
+        }
+    }
+
+    // inorder(left -> visit -> right)
+    void inorder(Node *root) {
+        inorder(root->left);
+        cout << root -> data;
+        inorder(root->right);
+    }
+
+    // preorder(visit -> left -> right)
+    void preorder(Node *root) {
+        cout << root -> data;
+        preorder(root->left);
+        preorder(root->right);
+    }
+
+    // post order(left -> right -> visit)
+    void postorder(Node *node) {
+        postorder(node->left);
+        postorder(node->right);
+        cout << node -> data;
+    }
+
+    //// search for node using its value
+    // using breadth-first
+    Node* search(int value, Node* root) {
+        while (!q.empty()) {
+            q.pop();
+        }
+        if (root == nullptr) {
+            cout << "Empty Tree";
+        }else {
+            q.push(root);
+            Node *current = root;
+            while (!q.empty()) {
+                current = q.front();
+                if (current -> data == value) {
+                    cout << current -> data;
+                    return current;
+                    break;
+                }else {
+                    q.pop();
+                    if (current -> left != nullptr) {
+                        q.push(current -> left);
+                    }
+                    if (current -> right != nullptr) {
+                        q.push(current -> right);
+                    }
+                }
+            }
+        }
+    }
+
+    //// delete node from the tree using its value
+
+    /* to find the explanation of this code you can go to hard-code playlist
+     *to find explanation of the logic of deletion you can visit Adel naseem or mycodeschool playlistz
+     * you can use findMin in right part (successor) instead of predecessor
+     */
+    Node *deleteNode(int value, Node *root) {
+        if (root == nullptr) {
+            cout << "Empty Tree";
+        }
+        else if (value < root -> data) {
+            root->left = deleteNode(value, root->left);
+        }
+        else if (value > root -> data) {
+            root->right = deleteNode(value, root->right);
+        }
+        else {
+            if (root -> left == nullptr && root -> right == nullptr) {
+                delete root;
+                root = nullptr;
+            }
+            else if (root -> right == nullptr) {
+                Node *temp = root->left;
+                delete root;
+                return temp;
+            }else if (root -> left == nullptr) {
+                Node *temp = root->right;
+                delete root;
+                return temp;
+            }else {
+                Node *predecessor = maxNode(root -> left);
+                root->data = predecessor -> data;
+                root->left = deleteNode(predecessor -> data, root->left);
+            }
+        }
+        return root;
+    }
+};
---
+++ stack.cpp
@@ -0,0 +1,282 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+template<typename T>
+
+//=======================================================================
+
+class Stack_array {
+private:
+    T arr[100];
+    // Last is the index of last element in the array
+    // we initialize it with -1
+    // the first index in stack has index 0 so is the index = -1 it's mean that the stack is empty
+    int last = -1;
+public:
+
+    bool isEmpty() {
+        if (last == -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    void push(T data) {
+        last++;
+        arr[last] = data;
+    }
+
+    void pop() {
+        last--;
+    }
+
+    T top() {
+        T top = arr[last];
+        return top;
+    }
+
+    void clear() {
+        last = -1;
+        cout << "Stack cleared!\n";
+    }
+
+    void display() {
+        for (int i = last; i >= 0; i--) {
+           cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+
+};
+
+//=======================================================================
+
+class Stack_linkedList {
+    struct node {
+        int data;
+        node *next = NULL;
+    };
+    node *top = NULL;
+
+    public:
+    void push(int data) {
+        node *newNode = new node;
+        newNode->data = data;
+
+        if (top == NULL) {
+            top = newNode;
+        }
+        else {
+            newNode->next = top;
+            top = newNode;
+        }
+    }
+
+    void pop() {
+        if (top == NULL) {
+            cout << "Stack is empty" << endl;
+        }else {
+            node *del = top;
+            top = top->next;
+            delete del;
+        }
+    }
+
+    void clear() {
+        while (top-> next != NULL) {
+            pop();
+        }
+        cout << "Stack cleared!\n";
+    }
+
+    void peek() {
+        int current = top->data;
+        cout << current << "\n";
+    }
+
+    void isEmpty() {
+        if (top == NULL) {
+            cout << "Stack is empty" << endl;
+        }else {
+            cout << "Stack is not empty" << endl;
+        }
+    }
+    void display() {
+        node *print = top;
+        while (print != NULL) {
+            cout << print->data << " ";
+            print = print->next;
+        }
+        cout << "\n";
+    }
+};
+
+//=======================================================================
+
+class Stack_vector {
+    vector<int> arr;
+    int size = -1;
+    public:
+    void push(int data) {
+        arr.push_back(data);
+        size++;
+    }
+
+    void pop() {
+        arr.pop_back();
+        size--;
+    }
+
+    void top() {
+        int current = arr[0];
+        cout << current << "\n";
+    }
+
+    void clear() {
+        arr.clear();
+        size = 0;
+        cout << "Stack cleared!\n";
+    }
+
+    void isEmpty() {
+        if (size == -1) {
+            cout << "Stack is empty" << endl;
+        }
+        else {
+            cout << "Stack is not empty" << endl;
+        }
+    }
+
+    void display() {
+        for (int i = size; i >= 0; i--) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+};
+
+//=======================================================================
+
+class Stack_doublyLinkedList {
+    struct node {
+        int data;
+        node *next = NULL;
+        node *prev = NULL;
+    };
+    node *top = NULL;
+    public:
+    void push(int data) {
+        node *newNode = new node;
+        newNode->data = data;
+        if (top == NULL) {
+            top = newNode;
+        }else {
+            newNode->next = top;
+            top = newNode;
+        }
+    }
+};
+
+//=======================================================================
+
+class check_brackets {
+    Stack_array<char> stack;
+    string input = "(iam asmaa), {this is my name},";
+public:
+    void check() {
+        for (char ch : input) {
+          if (ch == '(' || ch == '{' || ch == '[') {
+              stack.push(ch);
+          }else if (ch == ')' || ch == '}' || ch == ']') {
+              if (stack.isEmpty()) {
+                  cout << "not balanced" << endl;
+                  exit(0);
+              }else {
+                  char ch2 = stack.top();
+                  if (
+                      (ch2 == '(' && ch == ')') ||
+                      (ch2 == '{' && ch == '}') ||
+                      (ch2 == '[' && ch == ']')
+                      ) {
+                      stack.pop();
+                  }else {
+                      cout << "not balanced" << endl;
+                      exit(0);
+                  }
+              }
+          }
+        }
+
+        if (stack.isEmpty()) {
+            cout << "Balanced\n";
+        }
+        else {
+            cout << "not balanced\n";
+        }
+    }
+};
+
+//=======================================================================
+
+class postfix {
+    Stack_array<int> stack;
+    string x = "3 4 4 * + 5 8 * 4 / - 3 +";
+    public:
+    void evaluate(){
+        for (int i = 0; i < x.length(); i++) {
+            char ch = x[i];
+            if (ch == ' ') {
+                continue;
+            }else if (isdigit(ch)) {
+                stack.push(ch - '0');
+            }else if ((ch == '*') || (ch == '+') || (ch == '-') || (ch == '/')) {
+                int a = stack.top();
+                stack.pop();
+                int b = stack.top();
+                stack.pop();
+                if (ch == '*') {
+                    stack.push(a * b);
+                }else if (ch == '+') {
+                    stack.push(a + b);
+                }else if (ch == '-') {
+                    stack.push(a - b);
+                }else if (ch == '/') {
+                    stack.push(a / b);
+                }
+            }
+        }
+        cout << stack.top() << "\n";
+    }
+};
+int main() {
+
+    postfix p;
+    p.evaluate();
+    return 0;
+}
+
+//// Disadvantages fo implementing stack using array:
+
+/*
+  1 - you can't remove the element when you want to pop any element you can ignore it only, so it isn't an optimizing way for mrmory.
+  2 - has a static size you can't change it
+  3-  it's more possible to have overflow if you exceed the size of array
+  4- not flexibility
+*/
+
+//// Disadvantage for implementing stack using vector
+
+/*
+  it uses more memory than array(but less memory than linked list)
+*/
+
+//// Disadvantage for implementing stack using single linked list
+
+/*
+ 1 - it uses more memory than vector and array because of using pointers.
+ 2 - it wants more coding.
+ 3 - more difficult in implementing it.
+*/
+
