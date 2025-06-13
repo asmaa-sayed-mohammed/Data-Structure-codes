87c92233be13d15671440c6eeda371fc2200f8a1
Asmaa <saiedasmaa590@gmail.com>
2025-06-13 03:54:17 +0300

    max heap operations(extract max heap, increase key, insert into the heap)

--- heapSort.cpp
+++ heapSort.cpp
@@ -6,6 +6,7 @@ class heap {
     public:
     vector<int> array;
 
+    //initialize the vector
     heap(int size) {
         int element;
         for (int i = 0; i < size; i++) {
@@ -14,14 +15,22 @@ class heap {
         }
     }
 
+    // return the parent of a node
+    int parent(int i) {
+        return (i - 1) / 2;
+    }
+
+    // return the right child of the node
     int right(int i) {
         return i*2+2;
     }
 
+    //return the left child of the node
     int left(int i) {
         return i*2+1;
     }
 
+    // achieve heap conditions
     void maxHeapify(int i, int n) {
         int largest = i; //initialize the largest num as the index you have passed to function
         int l = left(i); // get the left child
@@ -40,6 +49,7 @@ class heap {
         }
     }
 
+    // convert the array to heap using maxheapify
     void buildMaxHeap() {
         // get the array size
         int arrSize = array.size();
@@ -49,6 +59,7 @@ class heap {
         }
     }
 
+    // sort the array using heap
     void heapSort() {
         buildMaxHeap();
         int arrSize = array.size();
@@ -61,7 +72,46 @@ class heap {
         }
     }
 
+    // get the largest element in the array
     int maxItem() {
         return array[0];
     }
+
+    // get the largest element in the array then delete it
+    int extractHeapMaximum() {
+        int arrSize = array.size();
+        if (arrSize == 0) {
+            cout<<"empty heap"<<endl;
+            return false;
+        }else {
+            int max = array[0]; // get the largest number of heap
+            swap(array[0], array[arrSize - 1]); // let it be the last node to delete it
+            array.pop_back(); // delete the max element
+            arrSize = array.size(); // update the size after deleting, or you can paa arrsize - 2 in maxHeapify
+            maxHeapify(0, arrSize - 1); // reheapify the array to be a valid heap
+            return max; // return the max element
+        }
+    }
+
+    // increase item in the heap and heapify it
+    void heapIncreaseKey(int i, int key) {
+        if (key < array[i]) {
+            cout<<"key is smaller than the old value"<<endl;
+        }else {
+            array[i] = key;
+            int parentIndex = parent(i);
+            while (i > 0 && array[parentIndex] > array[i]) {
+               swap(array[i], array[parentIndex]);
+                i = parentIndex;
+            }
+        }
+    }
+
+    // add new value to the heap and confirm that it is agreed with heap's conditions
+    void insert(int newValue) {
+        array.push_back(-1e9); // let the new element = - infinity then increase it to the new value
+        int arrSize = array.size();
+        heapIncreaseKey(arrSize - 1, newValue);
+    }
+
 };
